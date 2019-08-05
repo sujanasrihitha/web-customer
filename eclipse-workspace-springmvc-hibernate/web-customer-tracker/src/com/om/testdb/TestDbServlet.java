d34edb3988083025ad3713a66f5dfb0a37bc689a
kotha014 <sujanak014@gmail.com>
2019-08-05 10:08:39 -0700

    success

---
+++ eclipse-workspace-springmvc-hibernate/web-customer-tracker/src/com/om/testdb/TestDbServlet.java
@@ -0,0 +1,41 @@
+package com.om.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//set up connection variables
+		
+		String user="hbstudent";
+		String password="hbstudent";
+		String jdbcUrl ="jdbc:mysql://localhost:3306/springstudent?useSSL=false&serverTimezone=UTC";
+		String driver ="com.mysql.jdbc.Driver";
+		try {
+			
+			PrintWriter out = response.getWriter();
+			out.println("connecting to the databse"+jdbcUrl);
+			Class.forName(driver);
+			Connection myconn = DriverManager.getConnection(jdbcUrl, user, password);
+			out.println("CONNECTION SUCCESFULL");
+			myconn.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+}
